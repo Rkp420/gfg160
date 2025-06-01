4b11740a2a95e5234d5d584e3b146e45d90f58ed
Rkp420 <rkp20442@gmail.com>
2025-06-01 10:46:43 +0530

    Day 73 Solved

---
+++ Day73/remove_loop.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+
+struct Node
+{
+    int data;
+    Node* next;
+
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+
+class Solution
+{
+public:
+    // Function to remove a loop in the linked list.
+    void removeLoop(Node *head)
+    {
+
+        if (!head || !head->next)
+            return;
+
+        Node *slow = head;
+        Node *fast = head;
+
+        while (fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast)
+                break;
+        }
+
+        if (slow != fast)
+            return;
+
+        slow = head;
+        if (slow == fast)
+        {
+            while (fast->next != slow)
+            {
+                fast = fast->next;
+            }
+        }
+        else
+        {
+            while (slow->next != fast->next)
+            {
+                slow = slow->next;
+                fast = fast->next;
+            }
+        }
+
+        fast->next = NULL;
+    }
+};
+
+int main()
+{
+    return 0;
+}
