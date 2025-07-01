13d7654753abaa87b337c62b127d752c7413360b
Rkp420 <rkp20442@gmail.com>
2025-07-01 21:46:06 +0530

    Day 102 Solved

---
+++ Day102/stock_span.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+  public:
+    vector<int> calculateSpan(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> span(n);
+        stack<int> st;
+        
+        for(int i = 0; i < n; i++){
+            while(!st.empty() && arr[st.top()] <= arr[i]) {
+                st.pop();
+            }
+            
+            span[i] = st.empty() ? (i + 1) : (i - st.top());
+            st.push(i);
+        }
+        
+        return span;
+    }
+};
+
+int main()
+{
+    return 0;
+}
